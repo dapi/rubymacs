3fc62cfedc540859ee9be41dc38fc4095c4fe370
Danil Pismenny <danil@orionet.ru>
2010-02-24 18:13:49 +0300

    Use M-\ to exit auto-completion

--- my-completion.el
+++ my-completion.el
@@ -1,8 +1,9 @@
- ;;;
-  ;;;
-  ;;; autocomplete
-  ;;;
-  ;;;
+;;;
+;;;
+;;; autocomplete
+;;;
+;;;
+
 ;(setq load-path (cons "/usr/lib/ruby/gems/1.8/gems/rcodetools-0.8.5.0/" load-path))
 ;(require 'rcodetools)
 
@@ -12,19 +13,6 @@
 (require 'auto-complete-config)
 (require 'auto-complete-etags)
 
-; (defun ac-gtags-candidate2 ()
-;   (if (memq major-mode ac-gtags-modes)
-;         (ignore-errors
-;                 (with-temp-buffer
-;                           (when (eq (call-process "global" nil t nil "-ci" ac-prefix) 0)
-;                                       (goto-char (point-min))
-;                                                   (let (candidates)
-;                                                                 (while (and (not (eobp))
-;                                                                                           (push (buffer-substring-no-properties (line-beginning-position) (line-end-position)) candidates)
-;                                                                                                                     (eq (forward-line) 0)))
-;                                                                                                                                   candidates)))))) вместо nreverse acndidates
-;
-
 (add-to-list 'ac-gtags-modes 'ruby-mode)
 
 
@@ -32,10 +20,17 @@
 (setq ac-auto-start 1) ; 
 (setq ac-dwim t)
 
+;(define-key ac-completing-map "\r" 'ac-complete)
+(define-key ac-completing-map "\M-/" 'ac-stop)
+;(define-key ac-completing-map "\t" 'ac-complete)
+
+
+
 (add-hook 'ruby-mode-hook (lambda () (add-to-list 'ac-sources 'ac-source-gtags)))
 
 ; Постоянно сбоит
 ;(add-hook 'ruby-mode-hook (lambda () (add-to-list 'ac-sources 'ac-source-etags)))
+
 (add-hook 'ruby-mode-hook (lambda () (add-to-list 'ac-sources 'ac-source-words-in-same-mode-buffers)))
 (add-hook 'ruby-mode-hook (lambda () (add-to-list 'ac-sources 'ac-source-yasnippet)))
 (add-hook 'ruby-mode-hook (lambda () (add-to-list 'ac-sources 'ac-source-files-in-current-dir)))
@@ -47,40 +42,3 @@
 ; дурацкая штука	
 ;(require 'predictive)
 
-
-
-
-;;
-;;
-;; auto-complete
-;;
-;;
-
-
-;(add-hook 'auto-complete-mode-hook (lambda () (add-to-list 'ac-sources 'ac-source-filename)))
-;(global-auto-complete-mode t)
-;(setq ac-sources (append (list 'ac-source-filename) ac-sources))
-;(setq ac-sources (append (list 'ac-source-yasnippet) ac-sources))
-;(setq ac-sources (append (list 'ac-source-gtags) ac-sources))
-;(setq-default ac-sources 
-;              'ac-source-words-in-same-mode-buffers
-;	      )
-;              '(ac-source-yasnippet)
-;              '(ac-source-filename)
-;              '(ac-source-imenu))
-
-;(ac-rcodetools-initialize) ;; инициализация для Ruby
-
-;; (add-hook 'auto-complete-mode-hook (lambda () (add-to-list 
-;;                                                 'ac-sources 
-;;                                                 )))
-
-;;                                                'ac-source-filename
-
-;;                                                'ac-source-words-in-buffer
-;;                                                'ac-source-words-in-all-buffer
-;;                                                'ac-source-imenu
-;;                                                )))
-
-; (define-key ac-mode-map (kbd "M-TAB") 'auto-complete)
-;
