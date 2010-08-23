5e53c1986ea1903c23691fefe3754b226c244ecc
Danil Pismenny <danil@orionet.ru>
2010-08-23 15:34:36 +0400

    sass and other yasnippets added. js2 mode aded

---
+++ my-js.el
@@ -0,0 +1,17 @@
+;(add-to-list 'auto-mode-alist '("\\.js$" . js-mode))
+;; (add-hook 'js2-mode-hook 'my-coding-hook)
+
+; JS2
+; in Emacs, M-x byte-compile-file RE js2.el RE
+; http://code.google.com/p/js2-mode/
+;(autoload 'js2-mode "js2-mode" nil t)
+(require 'js2-mode)
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.js.erb$" . js2-mode))
+ 
+(setq js2-basic-offset 2)
+(setq js2-use-font-lock-faces t)
+
+
+
+
