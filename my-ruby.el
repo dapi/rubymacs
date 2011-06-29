c21e54f73afb4c23fbb0faa85c2b47c4f0f128fd
Danil Pismenny <danil@orionet.ru>
2011-06-29 16:38:00 +0400

    minor changes

--- my-ruby.el
+++ my-ruby.el
@@ -60,6 +60,7 @@
 (add-to-list 'auto-mode-alist '("\\.thor$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.irbrc$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Guardfile" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.ru$" . ruby-mode))
